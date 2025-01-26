3a6d865d4a8a9fba34bc391d996ad2b41f11a30c
Saladuit <saladuit@gmail.com>
2025-01-26 23:24:49 +0100

    Working on new spawning module |

--- src/systems/goods/spawn_animal_product.rs
+++ src/systems/goods/spawn_animal_product.rs
@@ -1,24 +1,32 @@
 use crate::bundles::*;
+use crate::components::Dimension;
 use crate::constants::*;
 
 use bevy::prelude::*;
 
 pub fn spawn_animal_product<'a>(
-    commands: &'a mut Commands,
-    animal_product: AnimalProductValues,
-    position: Vec3,
+  commands: &'a mut Commands,
+  animal_product: AnimalProductData,
+  position: Vec3,
 ) -> EntityCommands<'a> {
-    commands.spawn((AnimalProductBundle {
-        name: Name::new(animal_product.0),
-        tile: Tile::new(animal_product.1),
-        sprite: Sprite {
-            color: ANIMAL_PRODUCT_COLOR,
-            custom_size: Some(Vec2::new(
-                TILE_SIZE * animal_product.1 .0,
-                TILE_SIZE * animal_product.1 .1,
-            )),
-            ..Default::default()
-        },
-        transform: Transform::from_translation(position),
-    },))
+  commands.spawn(AnimalProductBundle {
+      tile: TileBundle {
+          name: Name::new(animal_product.name),
+          dimension: Dimension(animal_product.dimensions),
+          sprite: Sprite {
+              color: ANIMAL_PRODUCT_COLOR,
+              custom_size: Some(Vec2::new(
+                  TILE_SIZE * animal_product.dimensions.0,
+                  TILE_SIZE * animal_product.dimensions.1,
+              )),
+              ..Default::default()
+          },
+          transform: Transform::from_translation(position),
+          ..Default::default()
+      },
+      animal_product: AnimalProduct {
+          name: animal_product.name.to_string(),
+          dimensions: animal_product.dimensions,
+      },
+  })
 }
