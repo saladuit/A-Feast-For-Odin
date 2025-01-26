15dd6d00e1f10bd9a2e27d78640242cd51fbf5fd
Saladuit <saladuit@gmail.com>
2025-01-26 23:20:16 +0100

    Refactored still some logical errors. animals don't spawn in pregnant. Should add those componentns when they actually are

--- src/bundles/animal_bundle.rs
+++ src/bundles/animal_bundle.rs
@@ -2,10 +2,10 @@ use crate::bundles::*;
 use crate::components::*;
 use bevy::prelude::*;
 
+// TODO: Add pregnant when we have a way to handle it
 #[derive(Bundle)]
 pub struct AnimalBundle {
     pub animal_product: AnimalProductBundle,
     pub animal: Animal,
-    pub pregnant: Pregnant,
     pub victory_points: VictoryPoints,
 }
--- src/constants/animals.rs
+++ src/constants/animals.rs
@@ -4,9 +4,25 @@ use super::ANIMAL_PRODUCT_COLOR;
 
 pub const ANIMAL_COLOR: Color = ANIMAL_PRODUCT_COLOR;
 
-pub type AnimalValues = (&'static str, (f32, f32), bool, u32);
+pub struct AnimalData {
+    pub name: &'static str,
+    pub dimensions: (f32, f32),
+    pub victory_points: u32,
+}
 
-pub const SHEEP: AnimalValues = ("SHEEP", (2., 4.), false, 3);
-pub const CATTLE: AnimalValues = ("CATTLE", (3., 4.), false, 4);
+pub const SHEEP: AnimalData = AnimalData {
+    name: "SHEEP",
+    dimensions: (2.0, 4.0),
+    victory_points: 3,
+};
 
-pub const ANIMALS: &[AnimalValues] = &[SHEEP, CATTLE];
+pub const CATTLE: AnimalData = AnimalData {
+    name: "CATTLE",
+    dimensions: (3.0, 4.0),
+    victory_points: 4,
+};
+
+pub const ANIMALS: &[AnimalData] = &[
+    SHEEP,
+    CATTLE,
+];
